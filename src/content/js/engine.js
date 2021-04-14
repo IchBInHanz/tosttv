edd1b2c23ef8e07ac259e196ed5f27d3fb2313e9
IchBinHanz <nilssimons707@gmail.com>
2021-04-14 15:24:04 +0200

    v1.1
    Improved Engine

--- src/content/js/engine.js
+++ src/content/js/engine.js
@@ -7,75 +7,37 @@ const engine = async () => {
         safeBanner()
     }
 
-
-    var imgs_urls = await grabImgs(document)
-    if (old_imgs_urls.length !== imgs_urls.length) {
-        old_imgs_urls = imgs_urls
-        for(var imgs_url of imgs_urls) {
-            var img = await loadImage(imgs_url)
-            await model.classify(img, (error, results) => {
-                if (error) {
-                    console.error(error);
-                    return;
-                }
-                // console.log(results[0].label)
-                if (results[0].label == 'unsafe') {
-                    unsafe = true
-                    console.log(imgs_url+': Unsafe')
-                    img_info = {
-                        src: imgs_url,
-                        img: img,
-                        results: results,
-                        label: results[0].label
+    setInterval(async () => {
+        var imgs_urls = await grabImgs(document)
+        if (old_imgs_urls.length !== imgs_urls.length) {
+            old_imgs_urls = imgs_urls
+            for(var imgs_url of imgs_urls) {
+                var img = await loadImage(imgs_url)
+                await model.classify(img, async (error, results) => {
+                    if (error) {
+                        console.error(error);
+                        return;
                     }
-                    unsafe_imgs.push(img_info)
-                } else {
-                    console.log(imgs_url+': Safe')
-                }
-            });
+                    if (results[0].label == 'unsafe') {
+                        unsafe = true
+                        console.log(imgs_url+': Unsafe')
+                        img_info = {
+                            src: imgs_url,
+                            img: img,
+                            results: results,
+                            label: results[0].label
+                        }
+                        unsafe_imgs.push(img_info)
+                    } else {
+                        console.log(imgs_url+': Safe')
+                    }
+                });
+            }
+            if (unsafe) {
+                unsafeBanner(unsafe_imgs)
+            } else {
+                safeBanner()
+            }
         }
-        if (unsafe) {
-            unsafeBanner(unsafe_imgs)
-        } else {
-            safeBanner()
-        }
-    }
-
-
-
-
-    // setInterval(async () => {
-    //     var imgs_urls = await grabImgs(document)
-    //     if (old_imgs_urls.length !== imgs_urls.length) {
-    //         old_imgs_urls = imgs_urls
-    //         for(var imgs_url of imgs_urls) {
-    //             var img = await loadImage(imgs_url)
-    //             await model.classify(img, async (error, results) => {
-    //                 if (error) {
-    //                     console.error(error);
-    //                     return;
-    //                 }
-    //                 // console.log(results[0].label)
-    //                 if (results[0].label == 'unsafe') {
-    //                     unsafe = true
-    //                     console.log(imgs_url+': Unsafe')
-    //                     img_info = {
-    //                         src: imgs_url,
-    //                         img: img,
-    //                         results: results,
-    //                         label: results[0].label
-    //                     }
-    //                     unsafe_imgs.push(img_info)
-    //                 } else {
-    //                     console.log(imgs_url+': Safe')
-    //                 }
-    //             });
-    //         }
-    //         if (unsafe) {
-    //             unsafeBanner(unsafe_imgs)
-    //         } else {
-    //             safeBanner()
-    //         }
-    //     }
-    // }, 100);
+    }, 100);
 }
